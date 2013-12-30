f58dd57db974a53a38fe597cf86a449b3225963d
Daniel Stekhoven <daniel.stekhoven@scqm.ch>
2013-12-30 15:52:01 +0100

    Update date adjusted and tests removed

--- tests/classwtArgument.R
+++
@@ -1,9 +0,0 @@
-## Test for using the classwt argument                  Author: D.Stekhoven
-## ========================================================================
-## Created: 22.06.2012                                  Package: missForest
-library(missForest)
-dat <- data(iris)
-set.seed(13)
-xmis <- prodNA(iris)
-ximp <- missForest(xmis, ntree = 100, classwt = list(NULL, NULL, NULL, NULL, c(10, 30, 10)))
-summary(ximp$ximp); ximp$OOBerror
--- tests/strataArgument.R
+++
@@ -1,9 +0,0 @@
-## Test for using the strata argument                   Author: D.Stekhoven
-## ========================================================================
-## Created: 22.06.2012                                  Package: missForest
-library(missForest)
-dat <- data(iris)
-set.seed(13)
-xmis <- prodNA(iris)
-ximp <- missForest(xmis, ntree = 100, strata = list(NULL, NULL, NULL, NULL, iris$Species))
-summary(ximp$ximp); ximp$OOBerror
